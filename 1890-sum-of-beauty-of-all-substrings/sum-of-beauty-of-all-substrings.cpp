6560ec915c924e8f39552dc0aa4f53ae79de8f13
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-07-09 16:40:59 +0530

    Time: 162 ms (32.72%) | Memory: 11 MB (60.80%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int beauty(vector<int> &v){
+    int maxi=-1,mini=INT_MAX;
+    for(int i=0;i<26;i++){
+        maxi=max(maxi,v[i]);
+        if(v[i]>0){
+            mini=min(mini,v[i]);
+        }
+    }
+    return maxi-mini;
+}
+    int beautySum(string s) {
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<int> v(26,0);
+            for(int j=i;j<n;j++){
+                v[s[j]-'a']++;
+                ans+=beauty(v);
+            }
+        }
+        return ans;
+    }
+};
