0f63cbd231d86f7af649c7a6a36a64c1287a2390
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-07-22 15:25:32 +0530

    Time: 33 ms (40.77%) | Memory: 25.8 MB (71.51%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n=names.size();
-        priority_queue<pair<int,string>> pq;
+        unordered_map<int,string> mp;
         for(int i=0;i<n;i++){
-            pq.push({heights[i],names[i]});
+            mp[heights[i]]=names[i];
         }
-        vector<string> ans;
-        while(!pq.empty()){
-            auto i=pq.top();
-            pq.pop();
-            ans.push_back(i.second);
+        sort(heights.begin(),heights.end(),greater<int>());
+        for(int i=0;i<n;i++){
+            names[i]=mp[heights[i]];
         }
-        return ans;
+        return names;
 
     }
 };
