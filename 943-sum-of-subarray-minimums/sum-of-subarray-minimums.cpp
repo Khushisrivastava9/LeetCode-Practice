7c884b9feaf36dbf7e51385208f2a1a9c28a0d76
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-08-01 15:57:45 +0530

    Time: 70 ms (36.43%) | Memory: 45.8 MB (32.52%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,34 @@
+#define ll long long
+class Solution {
+public:
+int m=1e9+7;
+    int sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        stack<int> s1,s2;
+        vector<int> leftsmallestarr(n,-1);//indices of the smaller elements to it's left
+        vector<int> rightsmallestarr(n,n);//indices of the smaller elements to it's right
+        for(int i=0;i<n;i++){
+            while(!s1.empty() && arr[s1.top()]>arr[i]){
+                s1.pop();
+                
+            }
+            if(!s1.empty()){
+                leftsmallestarr[i]=s1.top();
+            }
+            s1.push(i);
+        }
+        for(int i=0;i<n;i++){
+            while(!s2.empty() && arr[s2.top()]>arr[i]){
+                rightsmallestarr[s2.top()]=i;
+                s2.pop();
+            }
+            s2.push(i);
+        }
+        ll sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += static_cast<ll>(i - leftsmallestarr[i]) * (rightsmallestarr[i] - i) * arr[i] % m;
+            sum %= m;
+        }
+        return sum;
+    }
+};
