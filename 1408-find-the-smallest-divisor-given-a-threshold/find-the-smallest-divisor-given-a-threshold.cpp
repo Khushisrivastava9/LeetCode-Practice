85c3e388c5f5ddb637ecb7ad50e34590080cb758
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-12-16 12:35:09 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (36.04%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+int fn(vector<int>& nums, int div){
+    int ans=0;
+    for(int i=0;i<nums.size();i++){
+        ans+=(nums[i]+div-1)/div;
+    }
+    return ans;
+}
     int smallestDivisor(vector<int>& nums, int threshold) {
         int n=nums.size();
         int s=1;
-        int e=1e9;
-        int ans=1;
+        int e=*max_element(nums.begin(),nums.end());
+        int ans=-1;
         while(s<=e){
-            int sum=0;
             int mid=s+(e-s)/2;
-            for(int i=0;i<n;i++){
-                int div=(nums[i]+mid-1)/mid;
-                sum+=div;
-            }
-            if(sum<=threshold){
-                e=mid-1;
-                ans=mid;
-            }else{
+            int sum=fn(nums,mid);
+            if(sum>threshold){
                 s=mid+1;
+            }else{
+                ans=mid;
+                e=mid-1;
             }
         }
         return ans;
