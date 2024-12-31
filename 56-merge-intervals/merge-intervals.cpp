6f961279ed8aa08c20635ccf154c20313d96829a
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-12-31 23:13:48 +0530

    Time: 4 ms (69.67%) | Memory: 23.9 MB (43.76%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,21 +3,19 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n=intervals.size();
         vector<vector<int>> ans;
-       sort(begin(intervals),end(intervals));
-       for(int i=0;i<n;i++){
-        int startidx=intervals[i][0];
-        int endidx=intervals[i][1];
-        if(!ans.empty() && endidx<=ans.back()[1]) continue;
-        for(int j=i+1;j<n;j++){
-            if(intervals[j][0]<=endidx){
-                endidx=max(endidx,intervals[j][1]);
+        sort(intervals.begin(),intervals.end());
+        int startidx=intervals[0][0];
+        int endidx=intervals[0][1];
+        for(int i=0;i<n;i++){
+            if(intervals[i][0]>endidx){
+                ans.push_back({startidx,endidx});
+                startidx=intervals[i][0];
+                endidx=intervals[i][1];
+            }else{
+                endidx=max(endidx,intervals[i][1]);
             }
-            else{
-                break;
-            }
-        }  
+        }
         ans.push_back({startidx,endidx});
-       } 
-       return ans;
+        return ans;
     }
 };
