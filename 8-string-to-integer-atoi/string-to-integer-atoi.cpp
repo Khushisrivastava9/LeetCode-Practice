1219570855207b6ae688fe174894a7b881b57899
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2025-05-26 22:40:20 +0530

    Time: 3 ms (15.93%) | Memory: 10.4 MB (5.21%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-int solve(int i,string s,long long ans,int sign){
-    if(i>=s.size() || !isdigit(s[i])) return ans*sign;
-    ans=ans*10+s[i]-'0';
-    if(ans>INT_MAX) return sign==-1?INT_MIN:INT_MAX;
-    return solve(i+1,s,ans,sign);
+    int solve(int i, string s, long long ans, int sign){
+        if(i >= s.size() || !isdigit(s[i])) return ans * sign;
+        ans = ans * 10 + (s[i] - '0');
+        if(ans * sign >= INT_MAX) return INT_MAX;
+        if(ans * sign <= INT_MIN) return INT_MIN;
 
-}
+        return solve(i + 1, s, ans, sign);
+    }
+
     int myAtoi(string s) {
-        int i=0;
-        int sign=1;
-        while(i<s.size() && s[i]==' ') i++;
-        if(s[i]=='+' || s[i]=='-') sign=44-s[i++];
-        return solve(i,s,0,sign);
+        int i = 0;
+        int sign = 1;
+        while(i < s.size() && s[i] == ' ') i++;
+        if(i < s.size()) {
+            if(s[i] == '-') {
+                sign = -1;
+                i++;
+            } else if(s[i] == '+') {
+                i++;
+            }
+        }
+        if(i >= s.size() || !isdigit(s[i])) return 0;
+        return solve(i, s, 0, sign);
     }
 };
