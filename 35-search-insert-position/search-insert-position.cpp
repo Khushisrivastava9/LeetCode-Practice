dd9daf13fff82b73aac6eecced4cf5013b0210fe
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2025-12-20 11:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (41.40%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,18 +4,17 @@ public:
         int n=nums.size();
         int s=0;
         int e=n-1;
-        int ans=0;
+        int ans=n;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target){
+            if(nums[mid]>=target){
                 ans=mid;
                 e=mid-1;
-            }else{
+            }
+            else{
                 s=mid+1;
             }
         }
-        if(nums[n-1]<target) ans=n;
         return ans;
     }
 };
